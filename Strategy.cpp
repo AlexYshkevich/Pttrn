9b6f032e45e0088adc016f550fbd49de13d6cf90
AlexYshkevich <bor_bor_1997@mail.ru>
2016-12-03 19:15:40 +0300

    Update Strategy.cpp

--- Strategy.cpp
+++ Strategy.cpp
@@ -10,41 +10,36 @@ public:
 class Strategy_1: public StrategyPay
 {
 public:
+	Strategy_1
 	void use(void){ std::cout << "Strategy_1" << std::endl; };
 };
 
-class Strategy_2: public Strategy
+class Strategy_2: public StrategyPay
 {
 public:
 	void use(void){ std::cout << "Strategy_2" << std::endl; };
 };
 
-class Strategy_3: public Strategy
-{
-public:
-	void use(void){ std::cout << "Strategy_3" << std::endl; };
-};
-
 class Context
 {
 protected:
-	Strategy* operation;
+	StrategyPay* operation;
 
 public:
 	virtual ~Context() {}
-	virtual void useStrategy(void) = 0;
-	virtual void setStrategy(Strategy* v) = 0;
+	virtual void useStrategyPay(void) = 0;
+	virtual void setStrategyPay(StrategyPay* v) = 0;
 };
 
 class Client: public Context
 {
 public:
-	void useStrategy(void)
+	void useStrategyPay(void)
 	{
 		operation->use();
 	}
 
-	void setStrategy(Strategy* o)
+	void setStrategyPay(StrategyPay* o)
 	{
 		operation = o;
 	}
@@ -55,14 +50,13 @@ int main(int /*argc*/, char* /*argv*/[])
 	Client customClient;
 	Strategy_1 str1;
 	Strategy_2 str2;
-	Strategy_3 str3;
 
-	customClient.setStrategy(&str1);
-	customClient.useStrategy();
-	customClient.setStrategy(&str2);
-	customClient.useStrategy();
-	customClient.setStrategy(&str3);
-	customClient.useStrategy();
+	customClient.setStrategyPay(&str1);
+	customClient.useStrategyPay();
+	customClient.setStrategyPay(&str2);
+	customClient.useStrategyPay();
+	customClient.setStrategyPay(&str3);
+	customClient.useStrategyPay();
 
 	return 0;
 }

ba7b3f36d090dc5ca37782053053ef909b1f0bd2
AlexYshkevich <bor_bor_1997@mail.ru>
2016-12-08 16:14:22 +0300

    Update 5_Prototype.cpp

--- 5_Prototype.cpp
+++ 5_Prototype.cpp
@@ -1,14 +1,34 @@
-class Meal {
+#include <iostream>
+#include <string>
+#include "stdafx.h"
+using namespace std;
+
+class BMW_E39
+{
 public:
-	virtual ~Meal();
-	virtual void eat() = 0;
-	virtual Meal *clone() const = 0;
-	//...
+	virtual BMW_E39 *clone() const = 0;
+	virtual void message()
+{
+        cout << "BMW E39 is base for the:";
+}
 };
-class Spaghetti : public Meal {
+
+class Imitator : public  BMW_E39
+{
 public:
-	Spaghetti( const Spaghetti &);
-	void eat();
-	Spaghetti *clone() const { return new Spaghetti( *this ); }
-	//...
+	Imitator();
+	Imitator *clone() const { return new Imitator( *this );	
 };
+
+int main()
+{
+	
+	Imitator objBMW_M5; // объявление объекта
+	objBMW_M5.message(); // вызов функции класса message
+  Context* p = new Context( new 1_Strategy);
+  p->compress( "file.txt");
+  system("pause");
+  return 0;
+}
+
+ 

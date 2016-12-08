5efc90431fbedc65730b1ca571586a10d159f8cb
AlexYshkevich <bor_bor_1997@mail.ru>
2016-12-08 15:42:21 +0300

    Create 5_Prototype.cpp

---
+++ 5_Prototype.cpp
@@ -0,0 +1,14 @@
+class Meal {
+public:
+	virtual ~Meal();
+	virtual void eat() = 0;
+	virtual Meal *clone() const = 0;
+	//...
+};
+class Spaghetti : public Meal {
+public:
+	Spaghetti( const Spaghetti &);
+	void eat();
+	Spaghetti *clone() const { return new Spaghetti( *this ); }
+	//...
+};

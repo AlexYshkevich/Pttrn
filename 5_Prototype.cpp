f7af9631928539a4dfda885fcd26179d93807767
AlexYshkevich <bor_bor_1997@mail.ru>
2016-12-08 17:18:58 +0300

    Update 5_Prototype.cpp

--- 5_Prototype.cpp
+++ 5_Prototype.cpp
@@ -1,34 +1,24 @@
 #include <iostream>
 #include <string>
-#include "stdafx.h"
 using namespace std;
-
-class BMW_E39
-{
+ 
+class BMW_E39{
 public:
 	virtual BMW_E39 *clone() const = 0;
-	virtual void message()
-{
+	virtual void message(){
         cout << "BMW E39 is base for the:";
 }
 };
-
+ 
 class Imitator : public  BMW_E39
 {
 public:
 	Imitator();
-	Imitator *clone() const { return new Imitator( *this );	
+	Imitator *clone() const { return new Imitator( *this )	
 };
-
-int main()
-{
-	
-	Imitator objBMW_M5; // объявление объекта
-	objBMW_M5.message(); // вызов функции класса message
-  Context* p = new Context( new 1_Strategy);
-  p->compress( "file.txt");
-  system("pause");
-  return 0;
-}
-
  
+int main(){
+	Imitator* BMW_M5; 
+	BMW_M5.message();
+	return 0;
+	};

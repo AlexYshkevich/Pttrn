677e980ef38ab80b707c7080b9031b2a8a9218d3
AlexYshkevich <bor_bor_1997@mail.ru>
2016-12-09 03:25:30 +0300

    Update 5_Prototype.cpp

--- 5_Prototype.cpp
+++ 5_Prototype.cpp
@@ -1,24 +1,57 @@
-#include <iostream>
-#include <string>
-using namespace std;
- 
-class BMW_E39{
+class Stooge {
 public:
-	virtual BMW_E39 *clone() const = 0;
-	virtual void message(){
-        cout << "BMW E39 is base for the:";
+   virtual Stooge* clone() = 0;
+   virtual void slap_stick() = 0;
+};
+
+class Factory {
+public:
+   static Stooge* make_stooge( int choice );
+private:
+   static Stooge* s_prototypes[4];
+};
+
+int main() {
+   vector roles;
+   int             choice;
+
+   while (true) {
+      cout << "Larry(1) Moe(2) Curly(3) Go(0): ";
+      cin >> choice;
+      if (choice == 0)
+         break;
+      roles.push_back(
+         Factory::make_stooge( choice ) );
+   }
+
+   for (int i=0; i < roles.size(); ++i)
+      roles[i]->slap_stick();
+   for (int i=0; i < roles.size(); ++i)
+      delete roles[i];
 }
-};
- 
-class Imitator : public  BMW_E39
-{
+
+class Larry : public Stooge {
 public:
-	Imitator();
-	Imitator *clone() const { return new Imitator( *this )	
+   Stooge*   clone() { return new Larry; }
+   void slap_stick() {
+      cout << "Larry: poke eyes\n"; }
 };
- 
-int main(){
-	Imitator* BMW_M5; 
-	BMW_M5.message();
-	return 0;
-	};
+class Moe : public Stooge {
+public:
+   Stooge*   clone() { return new Moe; }
+   void slap_stick() {
+      cout << "Moe: slap head\n"; }
+};
+class Curly : public Stooge {
+public:
+   Stooge*   clone() { return new Curly; }
+   void slap_stick() {
+      cout << "Curly: suffer abuse\n"; }
+};
+
+Stooge* Factory::s_prototypes[] = {
+   0, new Larry, new Moe, new Curly
+};
+Stooge* Factory::make_stooge( int choice ) {
+   return s_prototypes[choice]->clone();
+}
